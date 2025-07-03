f61c853b39b0b78b9694d8c2767a6d05f8cba804
vachan-maker <vachan@tutanota.com>
2025-07-03 22:56:06 +0530

    Ditched fpdf2

--- script.py
+++ script.py
@@ -1,2 +1,25 @@
 import streamlit as st
+from weasyprint import HTML, CSS
+from base64 import b64encode
+
+def gen_pdf():
+    html = HTML(string='''
+    <h1>The title</h1>
+    <p>Content goes here</p>
+    ''')
+    css = CSS(string='@page { size: A4; margin: 1cm }')
+    html.write_pdf('example.pdf', stylesheets=[css])
+
 st.title('Assignment Cover Generator')
+
+with st.form("form"):
+    name = st.text_input("Enter Your Name")
+    registerNumber = st.text_input("Enter Your Register Number")
+    subjectCode = st.text_input("Enter Subject Code")
+    subjectName = st.text_input("Enter Subject Name")
+    assignmentTopic = st.text_input("Assignment Topic")
+    submitted = st.form_submit_button("Submit")
+
+if submitted:
+    gen_pdf()
+
