1e5d3605c704fa67ad83acca5fffba02748e7d58
vachan-maker <vachan@tutanota.com>
2025-07-04 19:11:21 +0530

    Display pdf within streamlist, use jinja2 template to render the htm;

--- script.py
+++ script.py
@@ -2,21 +2,31 @@ import streamlit as st
 from weasyprint import HTML, CSS
 from base64 import b64encode
 from jinja2 import Environment, FileSystemLoader
+from streamlit_pdf_viewer import pdf_viewer
 
 def gen_pdf():
     environment = Environment(loader=FileSystemLoader("templates"))
     report = environment.get_template("cover.html")
-    html = HTML(string=report.render())
-    html.write_pdf('/temp/cover.pdf')
+    html = HTML(string=report.render(subject=subjectName,faculty=faculty,name=name,semester=semester,branch=branch,rollno=rollno))
+    css = CSS('templates/styles.css')
+    return html.write_pdf(stylesheets=[css])
 
+
+st.title("Assignment Cover Generator")
 with st.form("form"):
     name = st.text_input("Enter Your Name")
-    registerNumber = st.text_input("Enter Your Register Number")
-    subjectCode = st.text_input("Enter Subject Code")
-    subjectName = st.text_input("Enter Subject Name")
-    assignmentTopic = st.text_input("Assignment Topic")
+    rollno = st.text_input("Enter Your Register Number/Roll No:")
+    branch = st.text_input("Enter branch:",placeholder="Eg. CS/EC/EEE")
+    semester = st.number_input("Enter your current Sem:",min_value=1,max_value=10,step=1)
+    subjectCode = st.text_input("Enter Subject Code",placeholder="Eg. CST-201")
+    subjectName = st.text_input("Enter Subject Name",placeholder="Eg. Operating Systems")
+    faculty = st.text_input("Enter faculty name:",placeholder='Mr. Rajesh P.')
     submitted = st.form_submit_button("Submit")
 
 if submitted:
-    gen_pdf()
+    if not name:
+        st.error("Name is required")
+    st.success("Success!")
+    pdf_bytes = gen_pdf()
+    pdf_viewer(input=pdf_bytes, width=700, height=800)
 
